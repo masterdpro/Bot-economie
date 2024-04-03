da424d2b17f5d491b81e63c44c5cb1633874e883
masterdpro <youri.d2.bel@gmail.com>
2024-04-03 18:02:56 -0400

    ui

--- Src/Credentials/Config.js
+++ Src/Credentials/Config.js
@@ -1,5 +1,5 @@
 module.exports = {
     prefix: ["p!"],
-    botToken: "OTkxMDEzMDM3MDIyNTQzOTUz.GOH1GM.4ZpuMqsrVbTJpHrW9WG_JFUweyfrJESyh7ZB4U",
+    botToken: "0",
     ownerIds: ["722450303919587409"]
 };
